cdf29ae78c4425ef8cd0cb07b9214bd19e16aaba
eiire <bulax.d.2101@gmail.com>
2020-03-02 22:24:59 +0700

    "correct_algorithm_but_no_all_bug_fix"

--- Stepik/classes.py
+++ Stepik/classes.py
@@ -6,6 +6,8 @@ sys.stdin = open("input.txt", "r")
 
 class Hierarchy:
     dictinary_classes = {}
+    all_parents = []
+    lst_res = ""
 
     def add(self, name: string, parents=None) -> None:
         # As the name suggests, <filter> creates a list of elements for which a function returns true.
@@ -14,37 +16,41 @@ class Hierarchy:
         self.dictinary_classes.update({''.join(list(filter(lambda ch: ch != ' ', name))): parents.split()})
 
     def is_base_of(self, base: string, derived: string) -> bool:
-        for key in self.dictinary_classes:
-            if key == derived:  # don`t understand why need derived[0] but derived don`t work
-                for word in self.dictinary_classes[key]:
-                    if word == base:
-                        return 'Yes'
-        return 'No'
+        self.all_parents += self.dictinary_classes[derived]
+
+        for element_of_child in self.dictinary_classes[derived]:
+            self.is_base_of(base, element_of_child)
 
     def print(self):
         print(self.dictinary_classes)
 
+    def my_input(self):
+        n = int(sys.stdin.readline())
+        for i in range(n):
+            my_str = sys.stdin.readline()
+            if my_str.find(':') == -1:
+                name = my_str
+                object_hierarchy.add(name.replace('\n', ''), '')
+            else:
+                name, parents = my_str.split(":")
+                object_hierarchy.add(name, parents)
+
+        n_2 = int(sys.stdin.readline())
+        for i in range(n_2):
+            my_str = sys.stdin.readline().split(' ')
+            my_str[1] = ''.join(list(filter(lambda ch: ch != '\n', my_str[1])))
+            object_hierarchy.is_base_of(my_str[0], my_str[1])
+            fl = 0
+            print(self.all_parents)
+            for parents in self.all_parents:
+                if my_str[0] == parents:
+                    print('Yes')
+                    fl = 1
+            if fl == 0:
+                print('No')
+            self.all_parents = []  # clear list after query
+
 
 object_hierarchy = Hierarchy()
-
-
-def my_input():
-    n = int(sys.stdin.readline())
-    for i in range(n):
-        my_str = sys.stdin.readline()
-        if my_str.find(':') == -1:
-            name = my_str
-            object_hierarchy.add(name.replace('\n', ''), '')
-        else:
-            name, parents = my_str.split(":")
-            object_hierarchy.add(name, parents)
-
-    n_2 = int(sys.stdin.readline())
-    for i in range(n_2):
-        my_str = sys.stdin.readline().split(' ')
-        my_str[1] = ''.join(list(filter(lambda ch: ch != '\n', my_str[1])))
-        print(object_hierarchy.is_base_of(my_str[0], my_str[1]))
-
-
-my_input()
+object_hierarchy.my_input()
 object_hierarchy.print()
