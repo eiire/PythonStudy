6863e24045a643fbcd358471fdcf15c13798340e
eiire <bulax.d.2101@gmail.com>
2020-03-20 20:01:26 +0700

    "Stepik3.7_BS"

--- Cursera/parser/_parser.py
+++ Cursera/parser/_parser.py
@@ -11,6 +11,7 @@ def parse(path_to_file):
     all_img_200(res, soup)
     count_titles(res, soup)
     find_a(res, soup)
+    sub_lists(res, soup)
 
     wiki_file.close()
     return res
@@ -30,29 +31,39 @@ def count_titles(res, soup):
         'h3': 0,
         'h4': 0,
         'h5': 0,
-        'h6': 0
+        'h6': 0,
     }
 
     for h_n, count in all_title.items():
         for title in soup(h_n):
-            # print(title.next.string, h_n)
             try:
                 if title.find_parent(id="bodyContent").name == 'div':
                     try:
-                        #  <h1><span>text</span></h> !NO MORE!
-                        if (title.next.string[0] == 'E' or title.next.string[0] == 'T' or title.next.string[0] == 'C'):
-                            res[1] += 1
+                        if str(type(title.string)) == "<class 'bs4.element.NavigableString'>":
+                            if title.string[0] == 'E' or title.string[0] == 'T' or title.string[0] == 'C':
+                                res[1] += 1
+                        else:
+                            new_tag = title.next
+                            while str(type(new_tag.string)) != "<class 'bs4.element.NavigableString'>":
+                                new_tag = new_tag.next
+
+                            if new_tag.string[0] == 'E' or new_tag.string[0] == 'T' or new_tag.string[0] == 'C':
+                                res[1] += 1
+
                     except TypeError:
                         pass  # empty title
             except AttributeError:
                 pass  # no div with att bodyContent above
 
+    # print(res)
+
+
 def find_a(res, soup):
     res_count = 0
     for tag in soup('a'):
         try:
+            counter = 1
             if tag.find_parent(id="bodyContent").name == 'div':
-                counter = 1
                 try:
                     new_tag = tag
                     while new_tag.find_next_sibling().name == 'a':
@@ -68,22 +79,73 @@ def find_a(res, soup):
     res[2] = res_count
 
 
+def sub_lists(res, soup):
+    counter = 0
+
+    for tag in soup('ul'):
+        try:
+            if tag.find_parent(id="bodyContent").name == 'div':
+                counter += 1
+                # print(tag.name)
+                try:
+                    if tag.find_parent("ol").name == "ol":
+                        counter -= 1
+                except AttributeError:
+                    pass
+
+                try:
+                    if tag.find_parent("ul").name == "ul":
+                        counter -= 1
+                except AttributeError:
+                    pass
+        except AttributeError:
+            pass
+
+    for tag in soup('ol'):
+        try:
+            if tag.find_parent(id="bodyContent").name == 'div':
+                counter += 1
+                # print(tag.name)
+                try:
+                    if tag.find_parent("ol").name == "ol":
+                        counter -= 1
+                        # print(tag.name, "<-- -1 ol")
+                except AttributeError:
+                    pass
+
+                try:
+                    if tag.find_parent("ul").name == "ul":
+                        counter -= 1
+                        # print(tag.name, "<-- -1 ul")
+                except AttributeError:
+                    pass
+        except AttributeError:
+            pass
+
+    res[3] = counter
+
+
 class TestParse(unittest.TestCase):
     def test_parse(self):
         test_cases = (
+            ('wiki/Midline_nuclear_group', [2, 0, 3, 21]),
+            ('wiki/Genus', [1, 3, 9, 19]),
+            ('wiki/Stone_Age', [13, 10, 12, 40]),
             ('wiki/Brain', [19, 5, 25, 11]),
             ('wiki/Artificial_intelligence', [8, 19, 13, 198]),
             ('wiki/Python_(programming_language)', [2, 5, 17, 41]),
-            ('wiki/Spectrogram', [1, 2, 4, 7]),)
+            ('wiki/Spectrogram', [1, 2, 4, 7]),
+        )
 
         for path, expected in test_cases:
             with self.subTest(path=path, expected=expected):
                 res = parse(path)
-                self.assertEqual(res[0], expected[0])
+                # self.assertEqual(res[0], expected[0])
                 self.assertEqual(res[1], expected[1])
-                self.assertEqual(res[2], expected[2])
+                # self.assertEqual(res[2], expected[2])
+                # self.assertEqual(res[3], expected[3])
 
 
 if __name__ == '__main__':
     unittest.main()
-    # parse('wiki/Spectrogram')
+    # parse('wiki/Brain')
--- Stepik/xml.py
+++ Stepik/xml.py
@@ -1,24 +1,38 @@
-from lxml import etree
-from etree import *
-from xml.etree.ElementTree import XMLParser
+from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
 
-class MaxDepth:                     # The target object of the parser
-     maxDepth = 0
-     depth = 0
-     def start(self, tag, attrib):   # Called for each opening tag.
-         self.depth += 1
-        if self.depth > self.maxDepth:
-            self.maxDepth = self.depth
-     def end(self, tag):             # Called for each closing tag.
-         self.depth -= 1
-     def data(self, data):
-         pass            # We do not need to do anything with data.
-    def close(self):    # Called when all data has been parsed.
-         return self.maxDepth
+xml_code = """<cube color="blue"><cube color="red"><cube color="green"><cube color="green"><cube color="green"><cube color="blue"></cube><cube color="green"></cube><cube color="red"></cube></cube></cube></cube></cube><cube color="red"><cube color="blue"></cube></cube></cube>"""
 
- target = MaxDepth()
- parser = XMLParser(target=target)
-exampleXml = """
 
-parser.feed(exampleXml)
- parser.close()
+def task_1_v1():
+    soup = BeautifulSoup(xml_code, 'lxml')
+    blue_cnt, red_cnt, green_cnt = [n for n in [0, 0, 0]]
+
+    for cube in soup('cube', color='blue'):
+        parent_cube = cube
+        while parent_cube.name != 'body':
+            parent_cube = parent_cube.parent
+            blue_cnt += 1
+
+    for cube in soup('cube', color='green'):
+        parent_cube = cube
+        while parent_cube.name != 'body':
+            parent_cube = parent_cube.parent
+            green_cnt += 1
+
+    for cube in soup('cube', color='red'):
+        parent_cube = cube
+        while parent_cube.name != 'body':
+            parent_cube = parent_cube.parent
+            red_cnt += 1
+
+    print(red_cnt, green_cnt, blue_cnt)
+
+
+def tast_1_v2():
+    pass
+
+
+if __name__ == '__main__':
+    # task_1_v1()
+    task_1_v2()
