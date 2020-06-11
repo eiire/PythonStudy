fc416e9cc103c09521966e3b0cd44597c7c289e8
eiire <bulax.d.2101@gmail.com>
2020-06-12 00:30:18 +0700

    "Implementation of the effects system with class decorator"

--- Cursera/OOP/сreating_class_decorator/character.py
+++ Cursera/OOP/сreating_class_decorator/character.py
@@ -29,6 +29,10 @@ class Hero:
 
 
 class AbstractEffect(Hero, ABC):
+    def __init__(self, base_obj):
+        super().__init__()
+        self.base = base_obj
+
     @abstractmethod
     def get_negative_effects(self):
         pass
@@ -39,57 +43,32 @@ class AbstractEffect(Hero, ABC):
 
 
 class AbstractPositive(AbstractEffect):
-    def __init__(self, obj_hero):
-        super().__init__()
-        self.hero_with_effects = obj_hero
-        self.stats = obj_hero.stats
-        self.positive_effects = [effect for effect in obj_hero.positive_effects]
+    def get_positive_effects(self):
+        self.base.get_positive_effects()
+
+        self.positive_effects = [effect for effect in self.base.positive_effects]
+        self.positive_effects.append(type(self).__name__)
+
+        return self.positive_effects
 
 
 class AbstractNegative(AbstractEffect):
-    def __init__(self, obj_hero):
-        super().__init__()
-        self.hero_with_effects = obj_hero
-        self.stats = obj_hero.stats
-        self.negative_effects = [effect for effect in obj_hero.negative_effects]
-
-
-class Curse(AbstractPositive):
-    def get_stats(self):
-        for stat in self.stats.keys():
-            self.stats[stat] = self.stats[stat] - 2 \
-                if stat != 'HP' and stat != 'MP' and stat != 'SP' else self.stats[stat]
-
-        return self.stats
-
-    def get_positive_effects(self):
-        self.hero_with_effects.get_positive_effects()
-        return self.hero_with_effects.positive_effects
-
     def get_negative_effects(self):
+        self.base.get_negative_effects()
+
+        self.negative_effects = [effect for effect in self.base.negative_effects]
         self.negative_effects.append(type(self).__name__)
-        return self.negative_effects
 
-
-class Blessing(AbstractPositive):
-    def get_stats(self):
-        for stat in self.stats.keys():
-            self.stats[stat] += 2
-
-        return self.stats
-
-    def get_positive_effects(self):
-        self.positive_effects.append(type(self).__name__)
-        return self.positive_effects
-
-    def get_negative_effects(self):
         return self.negative_effects
 
 
 class Berserk(AbstractPositive):
     def get_stats(self):
+        self.base.get_stats()
+        self.stats = dict(self.base.stats)
+
         for stat in self.stats.keys():
-            self.stats[stat] = self.stats[stat] + 7 if stat == 'Strength' or stat == 'Endurance'\
+            self.stats[stat] = self.stats[stat] + 7 if stat == 'Strength' or stat == 'Endurance' \
                                                        or stat == 'Agility' or stat == 'Luck' else self.stats[stat]
             self.stats[stat] = self.stats[stat] - 3 if stat == 'Perception' or stat == 'Charisma' \
                                                        or stat == 'Intelligence' else self.stats[stat]
@@ -97,9 +76,67 @@ class Berserk(AbstractPositive):
 
         return self.stats
 
+    # like recursion
+    def get_negative_effects(self):
+        self.negative_effects = self.base.get_negative_effects()
+        return self.negative_effects
+
+
+class Curse(AbstractNegative):
+    def get_stats(self):
+        self.base.get_stats()
+        self.stats = dict(self.base.stats)
+
+        for stat in self.stats.keys():
+            self.stats[stat] = self.stats[stat] - 2 \
+                if stat != 'HP' and stat != 'MP' and stat != 'SP' else self.stats[stat]
+
+        return self.stats
+
     def get_positive_effects(self):
-        self.positive_effects.append(type(self).__name__)
+        self.positive_effects = self.base.get_positive_effects()
         return self.positive_effects
 
+
+class Blessing(AbstractPositive):
+    def get_stats(self):
+        self.base.get_stats()
+        self.stats = dict(self.base.stats)
+
+        for stat in self.stats.keys():
+            self.stats[stat] = self.stats[stat] + 2 \
+                if stat != 'HP' and stat != 'MP' and stat != 'SP' else self.stats[stat]
+
+        return self.stats
+
     def get_negative_effects(self):
+        self.negative_effects = self.base.get_negative_effects()
         return self.negative_effects
+
+
+class Weakness(AbstractNegative):
+    def get_stats(self):
+        self.base.get_stats()
+        self.stats = dict(self.base.stats)
+
+        for stat in self.stats.keys():
+            self.stats[stat] = self.stats[stat] - 4 \
+                if stat == 'Strength' or stat == 'Endurance' or stat == 'Agility' else self.stats[stat]
+
+        return self.stats
+
+    def get_positive_effects(self):
+        self.positive_effects = self.base.get_positive_effects()
+        return self.positive_effects
+
+
+class EvilEye(AbstractNegative):
+    def get_stats(self):
+        self.base.get_stats()
+        self.stats = dict(self.base.stats)
+        self.stats['Luck'] -= 10
+
+        return self.stats
+
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
--- Cursera/OOP/сreating_class_decorator/test_character.py
+++ Cursera/OOP/сreating_class_decorator/test_character.py
@@ -2,56 +2,89 @@ from unittest import TestCase
 
 
 class TestHero(TestCase):
-    def test_check_base_hero(self):
-        from Cursera.OOP.сreating_class_decorator.character import Hero
+    def test_decorator_removal(self):
+        from Cursera.OOP.сreating_class_decorator.character import Hero, AbstractPositive, Berserk, Curse
         hero = Hero()
-        print(
-            hero.stats, "- stats;\n",
-            hero.negative_effects, "- neg_eff;\n",
-            hero.positive_effects, "- pos_eff;\n\n",
-            hero.get_negative_effects(), "- get neg_eff;\n",
-            hero.get_positive_effects(), "- get pos_eff;\n",
-            hero.get_stats(), "- get stats\n",
-        )
+        brs1 = Berserk(hero)
+        brs2 = Berserk(brs1)
+        brs2.get_positive_effects()
+        # print(id(brs2), id(brs1))
+        cur1 = Curse(brs2)
+        # cur1.get_positive_effects()
+        print(cur1.positive_effects)
+        cur1.base = brs1  # remove second effect Berserk
+        # print(id(cur1.base))
+        self.assertEqual(id(brs1), id(cur1.base))
+        print(cur1.positive_effects)
 
-    def test_AbstractEffect(self):
-        from Cursera.OOP.сreating_class_decorator.character import Hero, AbstractEffect
-        stats_char = {
-            'HP': 128, 'MP': 42, 'SP': 100, 'Strength': 15, 'Perception': 4,
-            'Endurance': 8, 'Charisma': 2, 'Intelligence': 3, 'Agility': 8, 'Luck': 1
-        }
-        hero = Hero()
-
-        hero_with_effect = AbstractEffect(hero)
-
-        self.assertEqual(hero.stats, stats_char)
-        print(hero_with_effect.stats)
-        hero_with_effect.stats['HP'] = 0
-        print(hero_with_effect.stats)
-        print(hero.stats)
-
-    def test_AbstractPositive(self):
-        from Cursera.OOP.сreating_class_decorator.character import Hero, AbstractEffect, AbstractPositive, Blessing, Berserk
+    def test_pos_eff(self):
+        from Cursera.OOP.сreating_class_decorator.character import Hero, AbstractPositive, Berserk, Curse
+        # создадим героя
         hero = Hero()
 
-        hero_positive = Blessing(hero)
-        hero_positive.get_positive_effects()
-        print(hero_positive.positive_effects, 'base - ', hero.positive_effects)
+        brs1 = Berserk(hero)
+        brs1.get_positive_effects()
+        self.assertEqual(brs1.positive_effects, ['Berserk'], 'brs1')
+        brs2 = Berserk(brs1)
+        brs2.get_positive_effects()
+        self.assertEqual(brs2.positive_effects, ['Berserk', 'Berserk'], 'bsr2')
+        brs2.base = hero
+        brs2.get_positive_effects()
+        self.assertEqual(brs2.positive_effects, ['Berserk'], brs2.base.__class__)
 
-        hero_positive_2 = Blessing(hero_positive)
-        hero_positive_2.get_positive_effects()
-        print(hero_positive_2.positive_effects)
+        # cur1 = Curse(brs2)
+        #
+        # # 6 правильность добавления эффектов в список положительных эффектов
+        # print(cur1.get_positive_effects(), ['Berserk', 'Berserk'], '6')
+        # print('\n')
+        #
+        # # 7 правильность добавления эффектов в список отрицательных эффектов
+        # print(cur1.get_negative_effects(), '\n', ['Curse'], '7')
+        # print('\n')
+        # # снятие эффекта Berserk
+        # cur1.base = brs1
+        #
+        # # 9 правильность удаления эффектов из списка положительных эффектов
+        # print(cur1.get_positive_effects(), '\n', ['Berserk'], '9')
+        # print('\n')
 
-        hero_positive_2.hero_with_effects = hero_positive_2
-        print(hero_positive_2.positive_effects)
+    def test_stats_with_remove_effect(self):
+        from Cursera.OOP.сreating_class_decorator.character import Hero, AbstractPositive, Berserk, Curse
+        # создадим героя
+        hero = Hero()
 
-        g = Hero()
-        j = Berserk(g)
-        j.get_stats()
-        j.get_positive_effects()
-        k = Blessing(j)
-        k.get_positive_effects()
-        print(j.stats, j.positive_effects, '\n----', k.stats, k.positive_effects)
+        brs1 = Berserk(hero)
+
+        # 2 правильность изменения характеристик
+        # print(brs1.get_stats(), '\n', {'HP': 178,
+        #                                'MP': 42,
+        #                                'SP': 100,
+        #                                'Strength': 22,
+        #                                'Perception': 1,
+        #                                'Endurance': 15,
+        #                                'Charisma': -1,
+        #                                'Intelligence': 0,
+        #                                'Agility': 15,
+        #                                'Luck': 8}, '2')
+        print('\n')
+        brs1.get_stats()
+        brs1.get_positive_effects()
+        print('\n')
+        # повторное наложение эффекта Berserk
+        brs2 = Berserk(brs1)
+        brs2.base = hero
+        # print(brs1.stats, 'brs1')
+        self.assertEqual(brs2.get_stats(), {'HP': 178,
+                                            'MP': 42,
+                                            'SP': 100,
+                                            'Strength': 22,
+                                            'Perception': 1,
+                                            'Endurance': 15,
+                                            'Charisma': -1,
+                                            'Intelligence': 0,
+                                            'Agility': 15,
+                                            'Luck': 8}, "brs2 with remove one berserk")
+        # print(brs1.stats, 'brs1')
 
     def test_base_system(self):
         from Cursera.OOP.сreating_class_decorator.character import Hero, AbstractPositive, Berserk, Curse
@@ -61,85 +94,110 @@ class TestHero(TestCase):
         brs1 = Berserk(hero)
 
         # 2 правильность изменения характеристик
-        print(brs1.get_stats(), '\n', {'HP': 178,
-                                       'MP': 42,
-                                       'SP': 100,
-                                       'Strength': 22,
-                                       'Perception': 1,
-                                       'Endurance': 15,
-                                       'Charisma': -1,
-                                       'Intelligence': 0,
-                                       'Agility': 15,
-                                       'Luck': 8}, '2')
-        print('\n')
+        self.assertEqual(brs1.get_stats(), {'HP': 178,
+                                            'MP': 42,
+                                            'SP': 100,
+                                            'Strength': 22,
+                                            'Perception': 1,
+                                            'Endurance': 15,
+                                            'Charisma': -1,
+                                            'Intelligence': 0,
+                                            'Agility': 15,
+                                            'Luck': 8}, '2')
         # 3 неизменность списка отрицательных эффектов
-        print(brs1.get_negative_effects(), '\n', [], '3')
-        print('\n')
+        self.assertEqual(brs1.get_negative_effects(), [], '3')
 
         # 4 проверим, что в список положительных эффектов был добавлен Berserk
-        print(brs1.get_positive_effects(), '\n', ['Berserk'], '4')
-        print('\n')
+        self.assertEqual(brs1.get_positive_effects(), ['Berserk'], '4')
+
         # повторное наложение эффекта Berserk
         brs2 = Berserk(brs1)
-        print(brs2.get_stats(), "CHECK")
+
+        brs2.get_stats()
         # наложение эффекта Curse
         cur1 = Curse(brs2)
 
         # 5 правильность изменения характеристик
-        print(cur1.get_stats(), '\n', {'HP': 228,
-                                       'MP': 42,
-                                       'SP': 100,
-                                       'Strength': 27,
-                                       'Perception': -4,
-                                       'Endurance': 20,
-                                       'Charisma': -6,
-                                       'Intelligence': -5,
-                                       'Agility': 20,
-                                       'Luck': 13}, '5')
-        print('\n')
+        self.assertEqual(cur1.get_stats(), {'HP': 228,
+                                            'MP': 42,
+                                            'SP': 100,
+                                            'Strength': 27,
+                                            'Perception': -4,
+                                            'Endurance': 20,
+                                            'Charisma': -6,
+                                            'Intelligence': -5,
+                                            'Agility': 20,
+                                            'Luck': 13}, '5')
 
         # 6 правильность добавления эффектов в список положительных эффектов
-        print(cur1.get_positive_effects(), '\n', ['Berserk', 'Berserk'], '6')
-        print('\n')
+        self.assertEqual(cur1.get_positive_effects(), ['Berserk', 'Berserk'], '6')
 
         # 7 правильность добавления эффектов в список отрицательных эффектов
-        print(cur1.get_negative_effects(), '\n', ['Curse'], '7')
-        print('\n')
+        self.assertEqual(cur1.get_negative_effects(), ['Curse'], '7')
+
         # снятие эффекта Berserk
         cur1.base = brs1
 
         # 8 правильность изменения характеристик
-        print(cur1.get_stats(), '\n', {'HP': 178,
-                                       'MP': 42,
-                                       'SP': 100,
-                                       'Strength': 20,
-                                       'Perception': -1,
-                                       'Endurance': 13,
-                                       'Charisma': -3,
-                                       'Intelligence': -2,
-                                       'Agility': 13,
-                                       'Luck': 6}, '8')
-        print('\n')
+        self.assertEqual(cur1.get_stats(), {'HP': 178,
+                                            'MP': 42,
+                                            'SP': 100,
+                                            'Strength': 20,
+                                            'Perception': -1,
+                                            'Endurance': 13,
+                                            'Charisma': -3,
+                                            'Intelligence': -2,
+                                            'Agility': 13,
+                                            'Luck': 6}, '8')
 
         # 9 правильность удаления эффектов из списка положительных эффектов
-        print(cur1.get_positive_effects(), '\n', ['Berserk'], '9')
-        print('\n')
+        self.assertEqual(cur1.get_positive_effects(), ['Berserk'], '9')
 
         # 10 правильность эффектов в списке отрицательных эффектов
-        print(cur1.get_negative_effects(), '\n', ['Curse'], '10')
-        print('\n')
+        self.assertEqual(cur1.get_negative_effects(), ['Curse'], '10')
 
         # 11 незменность характеристик у объекта hero
-        print(hero.get_stats(), '\n', {'HP': 128,
-                                       'MP': 42,
-                                       'SP': 100,
-                                       'Strength': 15,
-                                       'Perception': 4,
-                                       'Endurance': 8,
-                                       'Charisma': 2,
-                                       'Intelligence': 3,
-                                       'Agility': 8,
-                                       'Luck': 1}, '11')
-        print('\n')
+        self.assertEqual(hero.get_stats(), {'HP': 128,
+                                            'MP': 42,
+                                            'SP': 100,
+                                            'Strength': 15,
+                                            'Perception': 4,
+                                            'Endurance': 8,
+                                            'Charisma': 2,
+                                            'Intelligence': 3,
+                                            'Agility': 8,
+                                            'Luck': 1}, '11')
 
+    def test_multiple_effects(self):
+        from Cursera.OOP.сreating_class_decorator.character import Hero, Curse, Berserk
+        hero = Hero()
+        print(hero.get_stats(), '<-- main', '\n')
 
+        b = Curse(Curse(Berserk(Berserk(hero))))
+        self.assertEqual(b.get_positive_effects(), ['Berserk', 'Berserk'])
+
+        ber = Berserk(Berserk(Berserk(hero)))
+        self.assertEqual(ber.get_stats()['HP'], 278)
+        self.assertEqual(ber.get_positive_effects(), ['Berserk', 'Berserk', 'Berserk'])
+
+    def test_killme(self):
+        from Cursera.OOP.сreating_class_decorator.character import Hero, AbstractPositive, Berserk, Curse
+        hero = Hero()
+        brs1 = Berserk(hero)
+        self.assertEqual(brs1.get_negative_effects(), [], '3')  # <-- был вызван
+        print(brs1.negative_effects)
+        brs2 = Berserk(brs1)
+        brs2.get_stats()
+        cur1 = Curse(brs2)
+        # self.assertEqual(cur1.get_negative_effects(), ['Curse'], '7')
+        cur1.base = brs1
+        print(cur1.negative_effects)
+        self.assertEqual(cur1.get_negative_effects(), ['Curse'], '10')
+
+    def test_3xCurs(self):
+        from Cursera.OOP.сreating_class_decorator.character import Hero, Curse, Berserk
+        hero = Hero()
+        curl = Curse(Curse(Curse(hero)))
+        ber = Berserk(Berserk(Berserk(hero)))
+
+        print(curl.get_negative_effects(), ber.get_positive_effects())
